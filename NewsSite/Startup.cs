27928e4d219de672d58327c9dfd6c0cd4e2c32f2
tomvrnak <tomvrnak@gmail.com>
2017-12-07 11:39:43 -0500

    first

---
+++ NewsSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsSite.Startup))]
+namespace NewsSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
